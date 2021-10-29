739ebeb94aae1e280c6559d94516979e6163a0f6
Stefan Knaak <stefan@knaak.org>
2021-10-29 23:01:33 +0200

    initial
    initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+#gc https://github.com/corgan2222/E-Shelter_SensorCount.git
+sudo apt install git python3 python3-pip pipenv
+python3 -m pip install --user pipenv
+pipenv install jupyterlab
+pipenv shell
+jupyter lab
+ 
